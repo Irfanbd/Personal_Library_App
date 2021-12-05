cb02434910db9b2c7a6e411113a0a71f7778f882
Irfanbd <gourob83@gmail.com>
2021-12-05 16:53:51 +0600

    Design

---
+++ P_Library/app/src/main/java/com/example/boipoks/DBHelper.java
@@ -0,0 +1,5 @@
+package com.example.boipoks;
+
+public class DBHelper {
+    
+}
--- P_Library/app/src/main/java/com/example/boipoks/MainActivity.java
+++ P_Library/app/src/main/java/com/example/boipoks/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.boipoks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+EditText name,WriterName,genre;
+Button insert,update,delete,view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
